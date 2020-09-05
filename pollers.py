1fc7352eb36ec4d1550d2f2ce76c3dc5f5c64935
Jimmy Li <jimmyli2002@gmail.com>
2020-09-05 19:28:47 +0000

    Added documentation to pollers, updated pollHTTP params

--- pollers.py
+++ pollers.py
@@ -6,6 +6,12 @@ import json
 import subprocess
 import hashlib
 
+"""
+Name: pollPort
+Description: Will poll for a specific port and see if it is online
+Parameters: ip - ip address to poll, port - port number
+"""
+
 def pollPort(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(3)
@@ -15,15 +21,26 @@ def pollPort(ip, port):
     else:
       return False
 
-def pollHTTP(ip, port, pageHash):
+"""
+Name: pollHTTP
+Description: Will query a url and verify that it is reachable and the md5 content of the page is what is expected
+Parameters: url - complete url of the page to query (ex. https://192.168.0.1/login.html), pageHash - expected md5 hash of the page
+"""
+
+def pollHTTP(url, pageHash):
     try:
-        if(hashlib.md5(requests.get("http://" + ip + ":" + port, timeout=3).content).hexdigest() == pageHash):
+        if(hashlib.md5(requests.get(url, timeout=3).content).hexdigest() == pageHash):
             return True
         else:
             return False
     except:
         return False
 
+"""
+Name: pollSSH
+Description: Will verify that the SSH service is running on the specific port
+Parameters: ip - ip address to poll, port - port number to poll, users - Array of strings of format "username:password" to verify are valid
+"""
 
 def pollSSH(ip, port, users):
     try:
@@ -38,6 +55,11 @@ def pollSSH(ip, port, users):
     except:
         return False
 
+"""
+Name: pollFTP
+Description: Will verify that the FTP service is running on the specific port
+Parameters: ip - ip address to poll, port - port number to poll, users - Array of strings of format "username:password" to verify are valid
+"""
 
 def pollFTP(ip, port, users):
     try:
